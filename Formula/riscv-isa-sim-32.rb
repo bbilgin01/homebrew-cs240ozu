bb66b1a6c3582e2d4ceda614fbf0eca9c6734d96
babdary <baturalp.bilgin@ozu.edu.tr>
2024-10-17 17:20:22 +0300

    file naming

---
+++ Formula/riscv-isa-sim-32.rb
@@ -0,0 +1,43 @@
+class RiscvIsaSim32 < Formula
+  desc "RISC-V ISA simulator (spike)"
+  homepage "http://riscv.org"
+  url "https://github.com/riscv/riscv-isa-sim.git"
+  version "main"
+
+  bottle do
+    root_url "http://riscv.org.s3.amazonaws.com/bottles"
+    rebuild 17
+    sha256 cellar: :any, sonoma: "11f640bb01a1b3ad864ab689257e1ae684b281f035d1343d9b415761e6b4cd23"
+    sha256 cellar: :any, arm64_sonoma: "471cd017418fb75b8b2f6eb6e6d79952f9741b7c6c70782267aa95fedff0b58f"
+  end
+
+  depends_on "dtc"
+  depends_on "boost" => :optional
+
+  opoo "Building with clang could fail due to upstream bug: https://github.com/riscv-software-src/riscv-isa-sim/issues/820" if build.with? "boost"
+
+  def install
+    mkdir "build"
+    cd "build" do
+      args = [
+       "--prefix=#{prefix}", "--with-target=../../riscv-pk/main/riscv32-unknown-elf"
+      ]
+      if build.with? "boost"
+        # This seems to be needed at least on macos/arm64
+        args << "--with-boost=#{Formula["boost"].prefix}"
+      else
+        args << "--without-boost"
+        args << "--without-boost-asio"
+        args << "--without-boost-regex"
+      end
+      # configure uses --with-target to set TARGET_ARCH but homebrew formulas only provide "with"/"without" options
+      system "../configure", *args 
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "false"
+  end
+end
